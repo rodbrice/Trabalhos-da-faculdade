a61d4c5da1d807c0d662f19d8b41b650cbacb5fc
rodbrice <roduit.brice.dev@gmail.com>
2026-03-29 00:26:50 +0100

    aa

--- Mobile-first_UI_Com_React/dr1_tp3.3/dev-bio-app/src/navigation/AppNavigator.js
+++ Mobile-first_UI_Com_React/dr1_tp3.3/dev-bio-app/src/navigation/AppNavigator.js
@@ -1,6 +1,6 @@
 ﻿// AppNavigator - Stack Navigator principal
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useTheme } from '../context/ThemeContext';
 
 // Screens
